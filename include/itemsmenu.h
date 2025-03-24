0fe68e523050355274c83e14b8d878d82113f689
RodrigoBurattoRibas <rodrigoburattoribasrbr@gmail.com>
2025-03-24 20:34:21 -0300

    Arrumando o Tipo das variáveis em itensmenu

--- include/itemsmenu.h
+++ include/itemsmenu.h
@@ -1,32 +1,37 @@
 #include<windows.h>
 
-int ItemsMenu(){
+void ItemsMenu(){
     system("cls");
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO windowInfo;
     GetConsoleScreenBufferInfo(console, &windowInfo);
     cout<<windowInfo.dwSize.X<<endl;
     cout<<windowInfo.dwSize.Y;
-    SetConsoleCursorPosition(console, {windowInfo.dwSize.X-1, 0});
-    int midY = windowInfo.dwSize.Y%2==0?windowInfo.dwSize.Y/2-1:windowInfo.dwSize.Y/2;
-    int midX = windowInfo.dwSize.X/2;
-    for (int i = 0; i < windowInfo.dwSize.Y; i++){
-        SetConsoleCursorPosition(console, {windowInfo.dwSize.X-1, i});
+    short int CalcRap = windowInfo.dwSize.X - 1;
+    SetConsoleCursorPosition(console, {CalcRap, 0});
+    short int midY = windowInfo.dwSize.Y%2==0?windowInfo.dwSize.Y/2-1:windowInfo.dwSize.Y/2;
+    short int midX = windowInfo.dwSize.X/2;
+    for (short int i = 0; i < windowInfo.dwSize.Y; i++){
+        SetConsoleCursorPosition(console, {CalcRap, i});
         cout<<"|";
         SetConsoleCursorPosition(console, {0, i});
         cout<<"|";
         if (i==midY) {
-            SetConsoleCursorPosition(console, {windowInfo.dwSize.X-4, i});
+            CalcRap = windowInfo.dwSize.X - 4;
+            SetConsoleCursorPosition(console, {CalcRap, i});
             cout<<"D ->";
             SetConsoleCursorPosition(console, {0, i});    
             cout<<"<- A";
         }
     }
-    SetConsoleCursorPosition(console, {midX-5, midY});  
+    midX -= 5;
+    SetConsoleCursorPosition(console, {midX, midY});  
     cout<<" __";
-    SetConsoleCursorPosition(console, {midX-5, midY+1});
+    midY +=1;
+    SetConsoleCursorPosition(console, {midX, midY});
     cout<<"/o \\_____";
-    SetConsoleCursorPosition(console, {midX-5, midY+2});
+    midY += 2;
+    SetConsoleCursorPosition(console, {midX, midY});
     cout<<"\\__/-=\"=\"`";
     getch();
 }
