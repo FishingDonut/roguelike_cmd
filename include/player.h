8fc0688d4c9588fe575f319af5cb7bfe5a2dd5d2
FishingDonut <protonsdimi@gmail.com>
2025-03-31 11:07:22 -0300

    loopPlayer

--- include/player.h
+++ include/player.h
@@ -51,7 +51,7 @@ struct Player
     {
         position.X += x;
         position.Y += y;
-    } 
+    }
 
     int updateHealth(int health)
     {
@@ -60,9 +60,69 @@ struct Player
     }
 };
 
-char getCharAtPosition(HANDLE hConsole, COORD position){
+char getCharAtPosition(HANDLE hConsole, COORD position)
+{
     char c;
     DWORD read;
     ReadConsoleOutputCharacterA(hConsole, &c, 1, position, &read);
     return c;
+}
+
+int loopPlayer()
+{
+    system("cls");
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD currentPosition = {0, 0};
+    COORD newPosition = {0, 0};
+    Player player = Player();
+    player.position = newPosition;
+
+    while (player.health > 0)
+    {
+        currentPosition = newPosition;
+        int a = getch();
+
+        if (a)
+        {
+            switch (a)
+            {
+            case 81:
+                SetConsoleCursorPosition(hConsole, player.position);
+                cout << "✞" << endl;
+                return 0;
+                break;
+            case 119:
+                newPosition.Y > 0 ? newPosition.Y-- : newPosition.Y;
+                break;
+            case 115:
+                newPosition.Y++;
+                break;
+            case 97:
+                newPosition.X > 0 ? newPosition.X-- : newPosition.X;
+                break;
+            case 100:
+                newPosition.X++;
+                break;
+            default:
+                cout << a << endl;
+                break;
+            }
+
+            SetConsoleCursorPosition(hConsole, {0, 32});
+            cout << getCharAtPosition(hConsole, newPosition);
+
+            if (getCharAtPosition(hConsole, newPosition) == '0')
+            {
+                newPosition = currentPosition;
+            }
+
+            player.setPosition(newPosition.X, newPosition.Y);
+            SetConsoleCursorPosition(hConsole, currentPosition);
+            cout << " ";
+            SetConsoleCursorPosition(hConsole, player.getPosition());
+            cout << "@";
+        }
+    }
+    return 0;
 }
