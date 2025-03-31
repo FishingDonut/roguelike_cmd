a92a8be08286caa1d9849463561658d1c279243a
FishingDonut <protonsdimi@gmail.com>
2025-03-31 11:07:52 -0300

    update main

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 #include "./include/maps.h"
 #include "./include/player.h"
 #include <locale.h>
+
 using namespace std;
-int key;
+
 int main()
 {
     SetConsoleTitle("RogueLike Game");
     system("cls");
-    //setlocale(LC_ALL, "Portuguese");
     SetConsoleOutputCP(CP_UTF8); // Definindo o console para usar caracteres UTF-8 (SAÍDA)
-    SetConsoleCP(CP_UTF8); // Definindo o console para usar caracteres UTF-8 (ENTRADA)
+    SetConsoleCP(CP_UTF8);       // Definindo o console para usar caracteres UTF-8 (ENTRADA)
     CONSOLE_SCREEN_BUFFER_INFO windowSize;
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
@@ -26,41 +26,46 @@ int main()
     GetConsoleScreenBufferInfo(out, &windowSize);
     SetConsoleCursorPosition(out, {0, 0});
     int loaded = 0;
+
     map mapteste = mapa();
-    for(int j = 0; j < 16; j++){
-        for(int i = 0; i < 16; i++){
+    
+    for (int j = 0; j < 16; j++)
+    {
+        for (int i = 0; i < 16; i++)
+        {
             switch (mapteste.map[j][i])
             {
-                case 0:
-                    cout << " ";
-                    break;
-                case mapteste.entities::parede:
-                    cout<< "\u2588";
-                    break;
-                case mapteste.entities::enemy:
-                    cout << "!";
-                    break;
-                case mapteste.entities::portaLat:
-                    cout << "\u007C";
-                    break;
-                case mapteste.entities::portaSupInf:
-                    cout << "\u2014";
-                    break;
-                case mapteste.entities::chest:
-                    cout << "\u00A4";
-                    break;
-                case mapteste.entities::mimic:
-                    cout << "\u00A4";
-                    break;
-                case mapteste.entities::vazio:
-                    cout << "\u0000";
-                    break;
-                default:
-                    break;
+            case 0:
+                cout << " ";
+                break;
+            case mapteste.entities::parede:
+                cout << "\u2588";
+                break;
+            case mapteste.entities::enemy:
+                cout << "!";
+                break;
+            case mapteste.entities::portaLat:
+                cout << "\u007C";
+                break;
+            case mapteste.entities::portaSupInf:
+                cout << "\u2014";
+                break;
+            case mapteste.entities::chest:
+                cout << "\u00A4";
+                break;
+            case mapteste.entities::mimic:
+                cout << "\u00A4";
+                break;
+            case mapteste.entities::vazio:
+                cout << "\u0000";
+                break;
+            default:
+                break;
             }
         }
         cout << endl;
     }
+
     if (windowSize.dwSize.X < 145)
     {
         SetConsoleTitle("Please Resize Game");
@@ -89,59 +94,7 @@ int main()
 
     if (menu() == 0)
     {
-        system("cls");
-        setlocale(LC_ALL, "pt_BR.UTF-8");
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        COORD currentPosition = {0, 0};
-        COORD newPosition = {0, 0};
-        Player player = Player();
-        player.position = newPosition;
-
-        while (true)
-        {
-            currentPosition = newPosition;
-            int a = getch();
-
-            if (a)
-            {
-                switch (a)
-                {
-                case 81:
-                    cout << "Saida!" << endl;
-                    return 0;
-                    break;
-                case 119:
-                    newPosition.Y > 0 ? newPosition.Y-- : newPosition.Y;
-                    break;
-                case 115:
-                    newPosition.Y++;
-                    break;
-                case 97:
-                    newPosition.X > 0 ? newPosition.X-- : newPosition.X;
-                    break;
-                case 100:
-                    newPosition.X++;
-                    break;
-                default:
-                    cout << a << endl;
-                    break;
-                }
-
-                SetConsoleCursorPosition(hConsole, {0, 32});
-                cout << getCharAtPosition(hConsole, newPosition);
-
-                if (getCharAtPosition(hConsole, newPosition) == '0')
-                {
-                    newPosition = currentPosition;
-                }
-
-                player.setPosition(newPosition.X, newPosition.Y);
-                SetConsoleCursorPosition(hConsole, currentPosition);
-                cout << " ";
-                SetConsoleCursorPosition(hConsole, player.getPosition());
-                cout << "@";
-            }
-        }
+        loopPlayer();
         return 0;
     }
     if (menu() == 2)
