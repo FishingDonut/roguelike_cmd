d6fbbde4c92c48a245b07a2d1a49e74af7994f31
FishingDonut <protonsdimi@gmail.com>
2025-04-04 20:33:44 -0300

    update

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ char getCharAtPosition(HANDLE hConsole, COORD position)
     return c;
 }
 
-void drawnMap(HANDLE &hConsole, int width, int height, int** matriz)
+void assembleMap(HANDLE &hConsole, int width, int height, int **matriz)
 {
     for (short i = 0; i < width; ++i)
     {
@@ -32,6 +32,31 @@ void drawnMap(HANDLE &hConsole, int width, int height, int** matriz)
     }
 }
 
+void drawnMap(HANDLE &hConsole, int width, int height, int **matriz)
+{
+    system("cls");
+    for (short i = 0; i < width; ++i)
+    {
+        for (short j = 0; j < height; ++j)
+        {
+            SetConsoleCursorPosition(hConsole, {i, j});
+            switch (matriz[i][j])
+            {
+            case 0:
+                cout << " ";
+                break;
+            case 1:
+                cout << "-";
+            case 2:
+                cout << "@";
+                break;
+            default:
+                cout << "?";
+                break;
+            }
+        }
+    }
+}
 int main()
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -39,6 +64,8 @@ int main()
     COORD newPosition = {10, 10};
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     Player player = Player();
+
+    int currentObjPosition;
 
     system("cls");
     SetConsoleOutputCP(CP_UTF8);
@@ -49,13 +76,17 @@ int main()
     int height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
     // Alocação dinâmica de matriz
-    int** map = new int*[width];
+    int **map = new int *[width];
     for (int i = 0; i < width; i++)
         map[i] = new int[height];
 
     player.position = newPosition;
 
+    assembleMap(hConsole, width, height, map);
     drawnMap(hConsole, width, height, map);
+
+    currentObjPosition = map[player.position.X][player.position.Y];
+    map[player.position.X][player.position.Y] = 2;
 
     while (player.health > 0)
     {
@@ -69,32 +100,21 @@ int main()
             case 81:
                 SetConsoleCursorPosition(hConsole, player.position);
                 cout << "✞" << endl;
-                
-                system("cls");
-
-                for (short i = 0; i < width; ++i)
-                {
-                    for (short j = 0; j < height; ++j)
-                    {            
-                        SetConsoleCursorPosition(hConsole, {i, j});
-                        cout << map[i][j];
-                    }
-                }
-
                 return 0;
                 break;
             case 119:
                 newPosition.Y > 0 ? newPosition.Y-- : newPosition.Y;
                 break;
             case 115:
-                newPosition.Y < height-1
-                 ? newPosition.Y++ : newPosition.Y;
+                newPosition.Y < height - 1
+                    ? newPosition.Y++
+                    : newPosition.Y;
                 break;
             case 97:
                 newPosition.X > 0 ? newPosition.X-- : newPosition.X;
                 break;
             case 100:
-                newPosition.X < width-1 ? newPosition.X++ : newPosition.X;
+                newPosition.X < width - 1 ? newPosition.X++ : newPosition.X;
                 break;
             default:
                 cout << keyBoard << endl;
@@ -104,16 +124,20 @@ int main()
             SetConsoleCursorPosition(hConsole, {0, 0});
             cout << getCharAtPosition(hConsole, newPosition);
 
-            if (getCharAtPosition(hConsole, newPosition) != ' ')
-            {
-                // newPosition = currentPosition;
-            }
-
+            currentObjPosition = map[newPosition.X][newPosition.Y];
             player.setPosition(newPosition.X, newPosition.Y);
-            SetConsoleCursorPosition(hConsole, currentPosition);
-            cout << " ";
-            SetConsoleCursorPosition(hConsole, player.position);
+
+            SetConsoleCursorPosition(hConsole, {currentPosition});
+            
+            map[currentPosition.X][currentPosition.Y] = currentObjPosition;
+            
+            cout << currentObjPosition;
+            
+            SetConsoleCursorPosition(hConsole, {player.position});
+            
             cout << "@";
+
+            map[player.position.X][player.position.Y] = 2;
         }
     }
     return 0;
