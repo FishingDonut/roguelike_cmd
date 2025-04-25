07a2ae855e8e9cd8b69786434264b95ba090c560
FishingDonut <protronsdimi@gmail.com>
2025-04-25 10:43:11 -0300

    boss

--- include/entity/boss.h
+++ include/entity/boss.h
@@ -14,7 +14,7 @@ struct Boss
     COORD position;
     COORD newPosition;
     COORD oldPosition;
-    
+
     Colors color = COLOR_CYAN;
     char skin = 'O';
     int health = 0;
@@ -22,11 +22,14 @@ struct Boss
     int currentObject = FLOOR;
     int previousObject = FLOOR;
     int valueMap = ENEMY;
+    int width = 3;
+    int height = 3;
     bool alive = true;
 
     void setPosition()
     {
-        if(!alive){
+        if (!alive)
+        {
             return;
         }
         position.X = newPosition.X;
@@ -36,14 +39,18 @@ struct Boss
 
     void updateHealth(int newhealth)
     {
-        if(!alive){
+        if (!alive)
+        {
             return;
         }
-        if(health <= 0){
+        if (health <= 0)
+        {
             clearBoss(*this);
             return;
         }
         health += newhealth;
         return;
     }
+    int centerY() { return (height / 2) + position.Y; }
+    int centerX() { return (width / 2) + position.X; }
 };
