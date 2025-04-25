7cb37947f7dad42d90c9609a0748c538ca4b85a7
FishingDonut <protronsdimi@gmail.com>
2025-04-25 09:04:17 -0300

    info beta complete

--- src/states/info_state.cpp
+++ src/states/info_state.cpp
@@ -3,6 +3,8 @@
 #include <conio.h>
 
 #include "states/info_state.h"
+#include "core/mapValueToChar.h"
+#include "core/render.h"
 #include "stateMachine.h"
 #include "GameData.h"
 
@@ -11,11 +13,26 @@ extern bool stateChanged;
 
 void info_enter()
 {
+    HANDLE hConsole = gameData.hConsole;
+    system("cls");
+    drawMargin(hConsole);
+    
+    SetConsoleCursorPosition(gameData.hConsole, {(SHORT)2, (SHORT)(height / 2) - 2});
+    std::cout << gameData.player.skin << " - Player";
+    SetConsoleCursorPosition(gameData.hConsole, {(SHORT)2, (SHORT)(height / 2) - 1});
+    std::cout << Enemy().skin << " - Enemy";
+    SetConsoleCursorPosition(gameData.hConsole, {(SHORT)2, (SHORT)(height / 2)});
+    std::cout << mapValueToChar(STAIR) << " - Stair";
+    SetConsoleCursorPosition(gameData.hConsole, {(SHORT)2, (SHORT)(height / 2) + 1});
+    std::cout << "As salas são geradas aleatoriamente, suba os andares até que o chefe aparessa, matando ele vc vence o jogo.";
     return;
 }
 
 void info_update()
 {
+    getch();
+    nextState = previousState;
+    stateChanged = true;
     return;
 }
 
