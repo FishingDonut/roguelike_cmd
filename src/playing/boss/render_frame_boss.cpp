0db3ec2042ba2a287f37887f84a49d23f14d4ae2
FishingDonut <protronsdimi@gmail.com>
2025-04-25 14:43:29 -0300

    render boss

--- src/playing/boss/render_frame_boss.cpp
+++ src/playing/boss/render_frame_boss.cpp
@@ -10,5 +10,24 @@ void render_frame_boss()
     if(!gameData.bossFloor){
         return;
     }
+
+    Boss &boss = gameData.boss;
+    auto& hConsole = gameData.hConsole;
+    auto& world = gameData.mapData.world;
+    
+    int initY = boss.position.Y - (boss.height / 2);
+    int initX = boss.position.X - (boss.width / 2);
+
+    for (int i = 0; i < boss.height; i++)
+    {
+        for (int j = 0; j < boss.width; j++){
+            int y = initY + i;
+            int x = initX + j;
+            
+            SetConsoleCursorPosition(hConsole, {(SHORT)x, (SHORT)y});
+            std::cout << colorChar(boss.color) << mapValueToChar(world[y][x]) << colorChar(COLOR_RESET);
+        }
+    }
+
     return;
 }
