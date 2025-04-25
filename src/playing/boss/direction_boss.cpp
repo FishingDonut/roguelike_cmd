58cbcad69a9d8d3e951904d97ea7b1eed7931051
FishingDonut <protronsdimi@gmail.com>
2025-04-25 16:37:54 -0300

    direction boss

--- src/playing/boss/direction_boss.cpp
+++ src/playing/boss/direction_boss.cpp
@@ -15,41 +15,37 @@ void direction_boss()
     int directionY = 0;
     int directionX = 0;
 
+    // Centro do boss é a posição
+    COORD center = position;
+
     if (room.isInRoom(player.position))
     {
         boss.color = COLOR_CYAN;
-        position.Y < player.newPosition.Y ? directionY = 1 : directionY = -1;
-        position.X < player.newPosition.X ? directionX = 1 : directionX = -1;
-
-        position.Y == player.newPosition.Y ? directionY = 0 : 0;
-        position.X == player.newPosition.X ? directionX = 0 : 0;
+        directionY = (player.newPosition.Y > center.Y) ? 1 : (player.newPosition.Y < center.Y) ? -1 : 0;
+        directionX = (player.newPosition.X > center.X) ? 1 : (player.newPosition.X < center.X) ? -1 : 0;
     }
-    else if (!room.isInRoom(position))
+    else if (!room.isInRoom(center))
     {
         boss.color = COLOR_RED;
-        position.Y < room.centerY() ? directionY = 1 : directionY = -1;
-        position.X < room.centerX() ? directionX = 1 : directionX = -1;
+        directionY = (room.centerY() > center.Y) ? 1 : (room.centerY() < center.Y) ? -1 : 0;
+        directionX = (room.centerX() > center.X) ? 1 : (room.centerX() < center.X) ? -1 : 0;
     }
     else
     {
         boss.color = COLOR_GREEN;
-        directionY = (rand() % 3) - 1;
+        directionY = (rand() % 3) - 1;  // -1, 0 ou 1
         directionX = (rand() % 3) - 1;
     }
 
+    // Limita movimento a uma direção por vez (X ou Y)
     if (directionY != 0 && directionX != 0)
     {
         if (rand() % 10 > 4)
-        {
             directionY = 0;
-        }
         else
-        {
             directionX = 0;
-        }
     }
 
     oldPosition = position;
-    newPosition = {(SHORT)(position.X + directionX), (SHORT)(position.Y + directionY)};
-    return;
+    newPosition = {(SHORT)(center.X + directionX), (SHORT)(center.Y + directionY)};
 }
