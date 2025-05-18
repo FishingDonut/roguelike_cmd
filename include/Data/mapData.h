07ed01273b2077f6660ed9cc8240bdf042436f3c
FishingDonut <protronsdimi@gmail.com>
2025-05-18 15:30:03 -0300

    update mapData use template

--- include/Data/mapData.h
+++ include/Data/mapData.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "global.h"
 #include "entity/room.h"
 #include "entity/trap.h"
 
+template <int h, int w>
 struct MapData
 {
     static constexpr int MAX_ROOMS = 12;
     static constexpr int MAX_TRAPS = 32;
-    int world[height][width];
+    int world[h][w];
     Room rooms[MAX_ROOMS];
     Trap traps[MAX_TRAPS];
     int countTraps = 0;
