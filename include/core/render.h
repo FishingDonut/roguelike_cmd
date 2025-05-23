c46cfc4362d85ab9e9818a493280bb698e70c896
FishingDonut <protronsdimi@gmail.com>
2025-05-23 20:06:46 -0300

    update score credits

--- include/core/render.h
+++ include/core/render.h
@@ -8,3 +8,4 @@ void drawMap(HANDLE &hConsole, int matriz[height][width]);
 void drawMargin(HANDLE &hConsole);
 void print_centered(short y, const std::string &text);
 void show_scoreboard();
+void capture_player_name(short y);
--- src/core/render.cpp
+++ src/core/render.cpp
@@ -75,3 +75,27 @@ void show_scoreboard()
     drawMargin(gameData.hConsole);
     getch();
 }
+
+void capture_player_name(short y)
+{
+    auto &name = gameData.player.name;
+    int c = 0;
+
+    while (c != 13) // Enter
+    {
+        c = getch();
+
+        if (c == 8 && !name.empty()) // Backspace
+        {
+            name.pop_back();
+        }
+        else if (isprint(c))
+        {
+            name += c;
+        }
+
+        COORD coord = {(SHORT)(width / 2 - name.length() / 2), y};
+        SetConsoleCursorPosition(gameData.hConsole, coord);
+        std::cout << " " << name << " ";
+    }
+}
