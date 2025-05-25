1d9621f419bc4bd2f721e61a5b7514172f6d24fc
FishingDonut <protronsdimi@gmail.com>
2025-05-25 16:59:28 -0300

    create Item

---
+++ include/entity/item.h
@@ -0,0 +1,34 @@
+#pragma once
+
+enum ItemType
+{
+    NONE,
+    POT_NONE,
+    POT_HP,
+    POT_XP,
+    POT_SPEED,
+    POT_MONEY,
+    POT_DIST,
+    POT_KILL,
+    POT_TRAP,
+};
+
+struct Item
+{
+    ItemType itemType = NONE;
+    char* name = "empty";
+    float value = 0;
+};
+
+const Item ITEM_DATABASE[] = {
+    { POT_HP, "Pot none", 0 },
+    { POT_HP, "Pot hp", 10 },
+    { POT_HP, "Pot xp", 20 },
+    { POT_HP, "Pot speed", 0.05 },
+    { POT_HP, "Pot money", 25 },
+    { POT_HP, "Pot dist", 1 },
+    { POT_HP, "Pot kill", 9999 },
+    { POT_HP, "Pot trap", 1 },
+};
+
+const int ITEM_DATABASE_SIZE = sizeof(ITEM_DATABASE) / sizeof(Item);
